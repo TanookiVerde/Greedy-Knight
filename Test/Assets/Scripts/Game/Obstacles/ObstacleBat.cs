d5dfe75a715d78a6d96f7919c1e5288753d36709
VDSMath <matheusvdsdf@gmail.com>
2018-05-05 01:24:40 -0300

    Obstáculos (Alpha)

---
+++ Test/Assets/Scripts/Game/Obstacles/ObstacleBat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class ObstacleBat : MonoBehaviour {
+
+    [Header("Movement Parameters")]
+    public float movementSpeed;
+
+    private Rigidbody2D myRB;
+    
+	void Start () {
+        myRB = GetComponent<Rigidbody2D>();
+        myRB.velocity = new Vector2(movementSpeed, 0);
+	}
+}
---
+++ Test/Assets/Scripts/Game/Obstacles/ObstacleSlow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSlow : MonoBehaviour {
+    public float velocityModifier;
+    private float velocity;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            CharacterMovement cm = collision.gameObject.GetComponent<CharacterMovement>();
+            velocity = cm.velocity;
+            cm.velocity *= velocityModifier;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            CharacterMovement cm = collision.gameObject.GetComponent<CharacterMovement>();
+            cm.velocity = velocity;
+        }
+    }
+}
