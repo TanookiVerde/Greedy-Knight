9216a59333490d37b546078bfaaa0875286682c3
TanookiVerde <pedrovitor.marques@gmail.com>
2018-06-09 10:26:23 -0300

    Inicio do Menu de Seleção de Fases

---
+++ One Tap Knight/Assets/Scripts/System/Levels/Level.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+    [Header("Level Information")]
+    [SerializeField] private bool isLocked;
+    [SerializeField] private int coinsAmount;
+    [Header("Player Information")]
+    [SerializeField] private int coinsAcquired;
+    [SerializeField] private bool gotAllCoins;
+    [SerializeField] private bool gotNoCoins;
+
+    public bool IsLocked()
+    {
+        return isLocked;
+    }
+    public Vector3 GetPointPosition()
+    {
+        return transform.position;
+    }
+    public void UnlockLevel()
+    {
+        isLocked = false;
+    }
+    public void GotAllCoins()
+    {
+        gotAllCoins = false;
+    }
+    public void GotNoCoins()
+    {
+        gotNoCoins = true;
+    }
+    public string SerializeLevelData()
+    {
+        return JsonUtility.ToJson(this);
+    }
+    public void DeserializeLevelDataToThisObject(string levelData)
+    {
+        Level data = JsonUtility.FromJson<Level>(levelData);
+        this.isLocked = data.isLocked;
+        this.gotAllCoins = data.gotAllCoins;
+        this.gotNoCoins = data.gotNoCoins;
+        this.coinsAcquired = data.coinsAcquired;
+    }
+}
---
+++ One Tap Knight/Assets/Scripts/System/Levels/World.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+    [SerializeField] private List<Level> levels;
+    [SerializeField] private Transform playerIcon;
+    [SerializeField] private LineRenderer lineProgression;
+
+    private void Start()
+    {
+        UpdateLineProgressionPosition();
+    }
+    private int GetLastUnlockedLevel()
+    {
+        for(int i = 0; i < levels.Count; i++)
+        {
+            if (levels[i].IsLocked())
+                return --i;
+        }
+        return levels.Count;
+    }
+    private void UpdateLineProgressionPosition()
+    {
+        lineProgression.SetPosition(1, levels[GetLastUnlockedLevel()].GetPointPosition());
+    }
+
+}
