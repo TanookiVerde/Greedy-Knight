31f928bd346015e6e739f1e8891a2376440d11fd
Pedro Nascimento <pedrovitor.marques@gmail.com>
2018-11-07 12:18:30 -0200

    Ajustes Main Menu

--- One Tap Knight/Assets/Scripts/System/UI/MainMenu/LevelSelectionPanel.cs
+++ One Tap Knight/Assets/Scripts/System/UI/MainMenu/LevelSelectionPanel.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelSelectionPanel : MenuPanel {
+    public GameObject levelButtonPrefab;
+    public Transform buttonRoot;
 
-	public void OpenLevel(int levelIndex)
+    public void Start()
+    {
+        CreateLevelButtons(MemoryCard.Load());
+    }
+    public void OpenLevel(int levelIndex)
     {
         Transition.transition.TransiteTo("TestScene");
+    }
+    private void CreateLevelButtons(AdventureLog log)
+    {
+        for(int i = 0; i < log.levels.Count; i++)
+        {
+            var b = Instantiate(levelButtonPrefab, buttonRoot);
+            b.GetComponent<LevelButton>().Initialize(log.levels[i], i);
+        }
     }
 }
